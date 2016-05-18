731bdb49eac747362d01483f9e6ec5c32b1a7c93
Ira Litvinenko <Ira Litvinenko>
2016-05-19 00:57:23 +0300

    pattern added

---
+++ src/com/Litvinenko/FigureFactory.java
@@ -0,0 +1,31 @@
+package com.Litvinenko;
+
+public class FigureFactory {
+
+    public GeometricFigure makeFigure(String type) {
+        GeometricFigure figure;
+
+        switch (type) {
+            case "circle":
+                figure = new Circle(0, 0, 1);
+                break;
+            case "triangle":
+                figure = new Triangle(0, 0, 0, 2, 2, 0);
+                break;
+            case "rectangle":
+                figure = new Rectangle(0, 0, 0, 2, 2, 2, 2, 0);
+                break;
+            case "rhombus":
+                figure = new Rhombus(0, 2, 1, 0, 0, -2, -1, 0);
+                break;
+            case "trapeze":
+                figure = new Trapeze(0, 0, 0, 2, 1, 2, 2, 0);
+                break;
+            default:
+                figure = new Circle(0, 0, 1);
+        }
+
+        return figure;
+
+    }
+}
--- src/com/Litvinenko/Main.java
+++ src/com/Litvinenko/Main.java
@@ -1,14 +1,18 @@
 package com.Litvinenko;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        FigureFactory figureFactory = new FigureFactory();
         GeometricFigure[] figures = new GeometricFigure[5];
-        figures[0] = new Circle(0, 0, 1);
-        figures[1] = new Triangle(0, 0, 0, 2, 2, 0);
-        figures[2] = new Rectangle(0, 0, 0, 2, 2, 2, 2, 0);
-        figures[3] = new Rhombus(0, 2, 1, 0, 0, -2, -1, 0);
-        figures[4] = new Trapeze(0, 0, 0, 2, 1, 2, 2, 0);
+
+        for (GeometricFigure figure : figures) {
+            String input = scanner.next();
+            figure = figureFactory.makeFigure(input);
+        }
 
 
         for (GeometricFigure figure : figures) {
